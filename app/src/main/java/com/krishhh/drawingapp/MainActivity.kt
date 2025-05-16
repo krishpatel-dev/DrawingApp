aa60fbba3abac2086046e75ac2ffcd7050881daf
Krish Patel <krishpatel2404@gmail.com>
2025-05-17 00:48:03 +0530

    commit

---
+++ app/src/main/java/com/krishhh/drawingapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.krishhh.drawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog
+    }
+
+}
